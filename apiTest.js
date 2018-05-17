875d9fb106c78e05d3ec99ffe9942ecd34bb415b
John Chappelle <jrchap82@gmail.com>
2018-05-17 13:33:04 -0400

    refactor

--- apiTest.js
+++ apiTest.js
@@ -2,17 +2,37 @@
 
 const url = `https://api.github.com/users/jrchapprogrammer/repos?type=owner`;
 let repos = [];
-let repoData = {};
+let apiDiv = document.getElementById("#api-container");
+apiDiv.innerHTML = "";
 
+function generateSuccessHTMLOutput(response) {
+  return (
+    "<h4>Result</h4>" +
+    "<h5>Status:</h5> " +
+    "<pre>" +
+    response.status +
+    " " +
+    response.statusText +
+    "</pre>" +
+    "<h5>Headers:</h5>" +
+    "<pre>" +
+    JSON.stringify(response.headers, null, "\t") +
+    "</pre>" +
+    "<h5>Data:</h5>" +
+    "<pre>" +
+    JSON.stringify(response.data, null, "\t") +
+    "</pre>"
+  );
+}
 function getRepos() {
   axios
-    .get(url)
-    .catch(error => console.log("BAD", error))
-    .then(response => console.log("GOOD", response));
+    .get(url, {
+      params: {
+        name: ""
+      }
+    })
+    .then(response => (apiDiv.innerHTML = generateSuccessHTMLOutput(response)));
 }
 
-repos = getRepos(url);
-console.log(repos[0]);
-
-const apiDiv = document.querySelector("#api-container");
-apiDiv.innerHTML = `<p>${repos}</p>`;
+getRepos();
+console.log(getRepos);
